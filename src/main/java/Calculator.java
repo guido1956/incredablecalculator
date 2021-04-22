ad2e96531ee3a88b9023c9e89980d5fb623f2075
Guido1956 <GuidoDulos@valori.nl>
2021-04-22 15:02:08 +0200

    US002 changed adding to optellen also in test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
     public Calculator() {
     }
 
-    public int adding(int number1, int number2) {
+    public int optellen(int number1, int number2) {
         return (number1 + number2);
     }
 
