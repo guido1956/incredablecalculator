417e56930552cb860e62230a234aeddc6ba14437
Guido1956 <GuidoDulos@valori.nl>
2021-04-22 12:58:08 +0200

    US003 method subtracking

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -5,6 +5,10 @@ public class Calculator {
     public int adding(int number1, int number2) {
         return (number1 + number2);
     }
+
+    public int subtrackting(int number1, int number2) {
+        return (number1 - number2);
+    }
 }
 
 
