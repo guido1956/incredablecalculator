b9c8eefa3e884a6ca0a0f9ce0ed139269e31b0a6
Guido1956 <GuidoDulos@valori.nl>
2021-04-22 13:19:43 +0200

    US003 method subtracking after tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -13,6 +13,16 @@ class CalculatorTest {
         int actualResult = calculator.adding(number1, number2);
         assertEquals(expectedResult, actualResult, "Adding 2 positive numbers went wrong");
     }
+
+    @DisplayName("Subtracting 2 positive numbers")
+    @Test
+    void testSubtractingWith2PositiveNumbersEndingInPositiveNumber() {
+        Calculator calculator = new Calculator();
+        int number1 = 60, number2 = 40;
+        int expectedResult = 20;
+        int actualResult = calculator.subtrackting(number1, number2);
+        assertEquals(expectedResult, actualResult, "Adding 2 positive numbers went wrong");
+    }
 }
 
 
